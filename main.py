800989031bc92f1722cb57d94663a7a154b58dc2
Hook <3366565v@mail.ru>
2022-12-17 17:46:09 +0300

    git innit

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+def get_intelligens(name):
+    response = requests.get('https://akabab.github.io/superhero-api/api/all.json').json()
+    for hero in response:
+        if hero['name'] == name:
+            return hero['powerstats']['intelligence']
+
+def SH_Choice():
+    heroes = ['Hulk', 'Captain America', 'Thanos']
+    rating_list = []
+    for n in heroes:
+        rating_list.append([get_intelligens(n), n])
+    rating_list.sort(reverse=True)
+    return f'{rating_list[0][1]} - это самый умный супергерой между: {", ".join(heroes)}'
+
+
+print(SH_Choice())
+
+
+
+# print(response)
