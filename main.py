ed9770a760341a493490c7f19d8df2a390df02dc
Hook <3366565v@mail.ru>
2022-12-22 15:21:49 +0300

    Task2

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 
+
 def get_intelligens(name):
     response = requests.get('https://akabab.github.io/superhero-api/api/all.json').json()
     for hero in response:
         if hero['name'] == name:
             return hero['powerstats']['intelligence']
+
 
 def SH_Choice():
     heroes = ['Hulk', 'Captain America', 'Thanos']
@@ -12,12 +14,38 @@ def SH_Choice():
     for n in heroes:
         rating_list.append([get_intelligens(n), n])
     rating_list.sort(reverse=True)
-    return f'{rating_list[0][1]} - это самый умный супергерой среди: {", ".join(heroes)}\n'\
+    return f'{rating_list[0][1]} - это самый умный супергерой среди: {", ".join(heroes)}\n' \
            f'Его уровень интеллекта {rating_list[0][0]} единиц'
 
 
 print(SH_Choice())
 
 
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
 
-# print(response)
+    def upload(self, path_to_file):
+
+        upload_link = requests.get('https://cloud-api.yandex.net:443/v1/disk/resources/upload',
+                                   headers={'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'},
+                                   params={'path': path_to_file}
+                                   )
+        response = requests.put(upload_link.json()['href'],
+                                headers={'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'},
+                                data=open(path_to_file, 'rb')
+                                )
+
+        print(response.status_code)
+        if response.status_code == 200 or 201:
+            print('Загрузка прошла успешно')
+
+
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = ...
+    token = ...
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
